3b5155f1f99411ab7a3f711187bf800dd95e30f0
Bruce <contato.consultoriacomproposito@gmail.com>
2026-08-29 12:17:44 -0300

    ajustes nos valores de paginação

--- src/pages/propostas/PropostasAvulsas.tsx
+++ src/pages/propostas/PropostasAvulsas.tsx
@@ -882,6 +882,9 @@ return (
             <option value={10}>10</option>
             <option value={20}>20</option>
             <option value={50}>50</option>
+            <option value={100}>100</option>
+            <option value={250}>250</option>
+            <option value={500}>500</option>
           </select>
           <span className="ml-2 text-slate-400">
             (Total: {propostasFiltradas.length} registros)
